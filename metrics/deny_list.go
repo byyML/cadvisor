202c0cf461d0aef175914f3e6c229f151155318c
Alex <18813197551@163.com>
2021-02-07 15:55:08 +0800

    bug fix

--- metrics/deny_list.go
+++ metrics/deny_list.go
@@ -1,8 +1,24 @@
+/*
+Copyright 2018 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
 package metrics
 
 import (
 	"regexp"
-	"strings"
 )
 
 // DenyList encapsulates the logic needed to filter based on a string.
@@ -13,10 +29,8 @@ type DenyList struct {
 
 // New constructs a new DenyList based on a white- and a
 // DenyList. Only one of them can be not empty.
-func New(b map[string]struct{}) (*DenyList, error) {
-	black := copyList(b)
-	var list map[string]struct{}
-	list = black
+func NewDenyList(b map[string]struct{}) (*DenyList, error) {
+	list := copyList(b)
 
 	return &DenyList{
 		list: list,
@@ -50,16 +64,6 @@ func (l *DenyList) IsIncluded(item string) bool {
 	return matched
 }
 
-// Status returns the status of the DenyList that can e.g. be passed into
-// a logger.
-func (l *DenyList) Status() string {
-	items := []string{}
-	for key := range l.list {
-		items = append(items, key)
-	}
-
-	return "DenyListing the following items: " + strings.Join(items, ", ")
-}
 
 func copyList(l map[string]struct{}) map[string]struct{} {
 	newList := map[string]struct{}{}
